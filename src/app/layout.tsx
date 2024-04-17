65b43aeec564a6deb6c23bc0f2f329b1a026fc19
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-04-17 16:37:50 +0300

    Add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,33 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next';
+import './globals.css';
 
 export const metadata: Metadata = {
-  title: "CarpTravel",
-  description: "Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. From thrilling mountain expeditions to peaceful forest walks, we offer unforgettable tours in one of Europe's most stunning mountain ranges. Learn more and join us on the journey of a lifetime with CarpTravel.",
+  title: 'CarpTravel',
+  description:
+    "Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. From thrilling mountain expeditions to peaceful forest walks, we offer unforgettable tours in one of Europe's most stunning mountain ranges. Learn more and join us on the journey of a lifetime with CarpTravel.",
+  icons: [
+    {
+      url: '/icons/favicon-dark.svg',
+      media: '(prefers-color-scheme: light)',
+      rel: 'icon',
+      type: 'image/svg',
+    },
+    {
+      url: '/icons/favicon-light.svg',
+      media: '(prefers-color-scheme: dark)',
+      rel: 'icon',
+      type: 'image/svg',
+    },
+  ],
+  // openGraph: {
+  //   type: 'website',
+  //   url: 'http://localhost:3000/',
+  //   title: 'CarpTravel',
+  //   description:
+  //     'Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. Learn more and join us on the journey of a lifetime with CarpTravel.',
+  //   siteName: 'CarpTravel',
+  //   images: [{ url: '/public/ogp/ogp.jpg' }],
+  // },
 };
 
 export default function RootLayout({
@@ -16,7 +37,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
