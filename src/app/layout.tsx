1482a7ac30e8096433d45042e23dd0b555545ebb
Nataliia Samokhvalova <samohvalova520@gmail.com>
2024-04-17 17:13:31 +0300

    Fixed metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
 
+const baseUrl = process.env.VERCEL_URL as string;
+
 export const metadata: Metadata = {
+  metadataBase: new URL(baseUrl),
   title: 'CarpTravel',
   description:
     "Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. From thrilling mountain expeditions to peaceful forest walks, we offer unforgettable tours in one of Europe's most stunning mountain ranges. Learn more and join us on the journey of a lifetime with CarpTravel.",
@@ -19,15 +22,23 @@ export const metadata: Metadata = {
       type: 'image/svg',
     },
   ],
-  // openGraph: {
-  //   type: 'website',
-  //   url: 'http://localhost:3000/',
-  //   title: 'CarpTravel',
-  //   description:
-  //     'Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. Learn more and join us on the journey of a lifetime with CarpTravel.',
-  //   siteName: 'CarpTravel',
-  //   images: [{ url: '/public/ogp/ogp.jpg' }],
-  // },
+  openGraph: {
+    title: 'CarpTravel',
+    description:
+      'Discover the breathtaking beauty of the Carpathian Mountains with CarpTravel. Learn more and join us on the journey of a lifetime with CarpTravel.',
+    url: `${baseUrl}`,
+    siteName: 'CarpTravel',
+    images: [
+      {
+        url: '/ogp/ogp.jpg',
+        width: 1200,
+        height: 600,
+        alt: 'Carpatian mountain view',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
